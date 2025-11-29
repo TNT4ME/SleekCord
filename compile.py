76c2899d00234950e92847727297816607998c0a
TNT4ME <lifeislife.256@gmail.com>
2025-11-30 03:27:41 +0530

    the problem is not a problem

--- compile.py
+++ compile.py
@@ -1,4 +1,4 @@
 from os import system as sys
 
-sys('sass src/import.scss dist/import.css --no-source-map')
-sys('sass src/main/easterEggs.scss dist/easterEggs.css --no-source-map')
+sys('sass src/import.scss dist/import.css')
+sys('sass src/main/easterEggs.scss dist/easterEggs.css')
